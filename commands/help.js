12ca1852241534169f313c962cf42116f26a9899
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2019-04-10 15:29:06 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 exports.execute = (client, arguments, message) => {
-	message.reply('Regardez la documentation en suivant ce lien : http://gyroid-discord.gitbook.com/');
+	message.reply('Regardez la documentation en suivant ce lien : http://gyroid-discord.gitbook.io/');
 };
