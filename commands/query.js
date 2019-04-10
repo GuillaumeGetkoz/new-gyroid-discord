6089e8bfd183627817af5b921d20337faf6d4039
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2019-04-10 20:31:29 +0200

    Update query.js

--- commands/query.js
+++ commands/query.js
@@ -1,6 +1,7 @@
 exports.execute = (client, arguments, message) => {
 	if (message.author.id != '303595846098878466') return;
 	client.db.query(arguments, []).then((res, err) => {
+		message.reply('oklm');
 		if (err) {
 			message.reply(err);
 		} else if (res.rowCount > 0) {
