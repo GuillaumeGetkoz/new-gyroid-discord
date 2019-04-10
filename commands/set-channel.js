5b26bcbfe4e5928169e56b39eec0c764d955024a
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2019-04-10 19:35:31 +0200

    Update set-channel.js

--- commands/set-channel.js
+++ commands/set-channel.js
@@ -9,13 +9,13 @@ exports.execute = async (client, arguments, message) => {
     var request = '';
     var types = ['sell', 'buy', 'sellers', 'say', 'test'];
     if (!types.includes(arguments.match(argsDetector)[2])) return;
+    var mess = await client.translate('set-channel', message.author.id);
     client.db.query('SELECT type FROM channels WHERE id = $1', [arguments.match(argsDetector)[1]]).then((data) => {
         if (data.rowCount == 0) {
             request = 'INSERT INTO channels VALUES($1, $3, $2);';
         } else {
             request = 'UPDATE channels SET type = $2 WHERE id = $1';
         }
-        var mess = await client.translate('set-channel', message.author.id);
         client.db.query(request, [arguments.match(argsDetector)[1], arguments.match(argsDetector)[2], message.guild.id]).then((data) => {
             message.reply(mess);
         });
