af396b38f82578567081749cf1e6040b99dd773d
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2019-04-14 15:16:03 +0200

    Update set-channel.js

--- commands/set-channel.js
+++ commands/set-channel.js
@@ -1,7 +1,7 @@
 exports.execute = async (client, arguments, message) => {
     message.channel.send('test');
     var argsDetector = /^<#([0-9]+)> (.+)$/;
-    if (arguments.match(argsDetector) == null) message.reply('go');
+    if (arguments.match(argsDetector) == null) return;
     if (!message.member.hasPermission('ADMINISTRATOR')) {
         var mess = await client.translate('noAdmin', message.author.id);
     	message.reply(mess);
@@ -9,7 +9,7 @@ exports.execute = async (client, arguments, message) => {
     }
     var request = '';
     var types = ['sell', 'buy', 'sellers', 'say', 'test'];
-    if (!types.includes(arguments.match(argsDetector)[2])) message.reply('hi');
+    if (!types.includes(arguments.match(argsDetector)[2])) return;
     var mess = await client.translate('set-channel', message.author.id);
     client.db.query('SELECT type FROM channels WHERE id = $1', [arguments.match(argsDetector)[1]]).then((data) => {
         if (data.rowCount == 0) {
