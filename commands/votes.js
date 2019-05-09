49a2f0ff70003570789798a03bf94b88574c2b14
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2019-05-09 18:02:31 +0200

    Update votes.js

--- commands/votes.js
+++ commands/votes.js
@@ -1,6 +1,7 @@
 exports.execute = (client, arguments, message) => {
 	if (message.author.id != '303595846098878466') return;
 	client.db.query('SELECT * FROM votes').then((votes) => {
+		var mess = '';
 		votes.rows.forEach((val) => {
 			mess += '- <@' + val.member + '> a voté pour Gyroïd à' + val.moment + '\n';
 		});
