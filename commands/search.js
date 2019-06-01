e0c0ed1e593dcf926ac94d345d9353842957a069
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2019-06-01 11:47:24 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,4 +1,5 @@
 exports.execute = async (client, arguments, message) => {
     var buy = require('../channels/buy');
-    buy.execute(client, message.content.replace('!market search ', ''));
+    message.content = message.content.replace('!market search ', '');
+    buy.execute(client, message);
 };
